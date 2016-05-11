3560b6d9d6a49e3009a2d41d1974f49b6aebcf2c
satojkovic <satojkovic@gmail.com>
2016-05-12 08:17:57 +0900

    Add basic model parameters

--- cifar10.py
+++ cifar10.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import division
+from __future__ import print_function
+
 import tensorflow as tf
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_integer('batch_size', 128,
+                            """Number of images to process in batch""")
+tf.app.flags.DEFINE_string('data_dir', '/tmp/cifar10_data',
+                           """Path to the CIFAR-10 data directory""")
 
 
 def main():
