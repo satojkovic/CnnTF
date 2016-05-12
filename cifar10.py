2c9ee4505c191e3d24f4e6695c49f2be47cddf31
satojkovic <satojkovic@gmail.com>
2016-05-13 08:04:28 +0900

    Add global constants describing the dataset

--- cifar10.py
+++ cifar10.py
@@ -8,10 +8,17 @@ import tensorflow as tf
 
 FLAGS = tf.app.flags.FLAGS
 
+# Basic model parameters
 tf.app.flags.DEFINE_integer('batch_size', 128,
                             """Number of images to process in batch""")
 tf.app.flags.DEFINE_string('data_dir', '/tmp/cifar10_data',
                            """Path to the CIFAR-10 data directory""")
+
+# Global constants describing the CIFAR-10 data set.
+IMAGE_SIZE = 24
+NUM_CLASSES = 10
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
 
 
 def main():
