598dd3feb8f6a3702aa4777a8c1327a148198dbb
satojkovic <satojkovic@gmail.com>
2016-05-14 11:42:51 +0900

    Add a download method

--- cifar10.py
+++ cifar10.py
@@ -5,6 +5,10 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+import os
+import sys
+from six.moves import urllib
+import tarfile
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -20,10 +24,24 @@ NUM_CLASSES = 10
 NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
 NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
 
-
-def main():
-    pass
+DATA_URL = 'http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz'
 
 
-if __name__ == '__main__':
-    main()
+def maybe_download_and_extract():
+    dest_directory = FLAGS.data_dir
+    if not os.path.exists(dest_directory):
+        os.makedirs(dest_directory)
+    filename = DATA_URL.split('/')[-1]
+    filepath = os.path.join(dest_directory, filename)
+    if not os.path.exists(filepath):
+
+        def _progress(count, block_size, total_size):
+            sys.stdout.write('\r>> Downloading %s %.1f%%' % (filename, float(
+                count * block_size) / float(total_size) * 100.0))
+            sys.stdout.flush()
+
+        filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
+        print()
+        statinfo = os.stat(filepath)
+        print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
+        tarfile.open(filepath, 'r:gz').extractall(dest_directory)
