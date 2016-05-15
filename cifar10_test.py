09e272b9d19b86f6f756d396bab4c312ff152d3d
satojkovic <satojkovic@gmail.com>
2016-05-15 14:37:45 +0900

    Add distorted_inputs test

--- cifar10_test.py
+++ cifar10_test.py
@@ -4,13 +4,21 @@
 import unittest
 import cifar10
 import os
+import tensorflow as tf
 
 
-class TestCifar10(unittest.TestCase):
+class TestCifar10(tf.test.TestCase):
     def test_maybe_download(self):
         cifar10.maybe_download_and_extract()
         self.assertEqual(
             True, os.path.exists('/tmp/cifar10_data/cifar-10-binary.tar.gz'))
+
+    def test_distorted_inputs(self):
+        images, labels = cifar10.distorted_inputs()
+        with self.test_session() as sess:
+            batch_size, image_size, _, _ = images.shape
+            self.assertEqual(128, batch_size)
+            self.assertEqual(24, image_size)
 
 
 if __name__ == '__main__':
