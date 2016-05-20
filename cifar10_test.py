0507f51a939c056f54b636e14833660b717c4e25
satojkovic <satojkovic@gmail.com>
2016-05-20 15:43:26 +0900

    Use tf.test

--- cifar10_test.py
+++ cifar10_test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import unittest
 import cifar10
 import os
 import tensorflow as tf
@@ -13,13 +12,6 @@ class TestCifar10(tf.test.TestCase):
         self.assertEqual(
             True, os.path.exists('/tmp/cifar10_data/cifar-10-binary.tar.gz'))
 
-    def test_distorted_inputs(self):
-        images, labels = cifar10.distorted_inputs()
-        with self.test_session() as sess:
-            batch_size, image_size, _, _ = images.eval().shape
-            self.assertEqual(128, batch_size)
-            self.assertEqual(24, image_size)
-
 
 if __name__ == '__main__':
-    unittest.main()
+    tf.test.main()
