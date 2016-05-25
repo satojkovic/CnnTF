130dd896a847d451ff2357f6b92c1b4c67f2dfbb
satojkovic <satojkovic@gmail.com>
2016-05-26 08:22:00 +0900

    Implement train method

--- cifar10_train.py
+++ cifar10_train.py
@@ -12,6 +12,29 @@ tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_train',
                            """and checkpoint.""")
 
 
+def train():
+    with tf.Graph().as_default():
+        global_step = tf.Variable(0, trainable=False)
+
+        images, labels = cifar10.distorted_inputs()
+
+        logits = cifar10.inference(images)
+
+        loss = cifar10.loss(logits, labels)
+
+        train_op = cifar10.train(loss, global_step)
+
+        saver = tf.train.Saver(tf.all_variables())
+
+        summary_op = tf.merge_all_summaries()
+
+        init = tf.initialize_all_variables()
+
+        sess = tf.Session(config=tf.ConfigProto(log_device_placement=
+                                                FLAGS.log_device_placement))
+        sess.run(init)
+
+
 def main():
     cifar10.maybe_download_and_extract()
     if tf.gfile.Exists(FLAGS.train_dir):
