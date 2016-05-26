a038a7039856e51a04784f63deae5670dfa7efd2
satojkovic <satojkovic@gmail.com>
2016-05-27 08:35:29 +0900

    Implement train method

--- cifar10_train.py
+++ cifar10_train.py
@@ -4,8 +4,10 @@
 import cifar10
 import tensorflow as tf
 import numpy as np
+import FLAGS
+import datetime
 
-FLAGS = tf.app.flags.FLAGS
+time = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_train',
                            """Directory where to write event logs""",
@@ -34,6 +36,35 @@ def train():
                                                 FLAGS.log_device_placement))
         sess.run(init)
 
+        tf.train.start_queue_runners(sess=sess)
+
+        summary_writer = tf.train.SummaryWriter(FLAGS.train_dir, sess.graph)
+
+        for step in xrange(FLAGS.max_steps):
+            start_time = time.time()
+            _, loss_value = sess.run([train_op, loss])
+            duration = time.time() - start_time
+
+            assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+
+            if step % 10 == 0:
+                num_examples_per_step = FLAGS.batch_size
+                examples_per_sec = num_examples_per_step / duration
+                sec_per_batch = float(duration)
+
+                format_str = (
+                    '%s: step %d, loss = %.2f (%.1f examples/sec; %.3f )'
+                    'sec/batch')
+                print(format_str % (datetime.now(), step, loss_value,
+                                    examples_per_sec, sec_per_batch))
+
+            if step % 100 == 0:
+                summary_str = sess.run(summary_op)
+                summary_writer.add_summary(summary_str, step)
+
+            if step % 1000 = 0 or (step + 1) == FLAGS.max_steps:
+                checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
+                saver.save(sess, checkpoint_path, global_step=step)
 
 def main():
     cifar10.maybe_download_and_extract()
